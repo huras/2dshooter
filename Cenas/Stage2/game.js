a5ede3828872b31cabbfc4dc64ed0ccaba827517
jean <jean@criasol>
2020-08-11 19:17:49 -0300

    alteradas as operações para tabuada de 5 6 e 7 e cores do fundo no stage 2

--- Cenas/Stage2/game.js
+++ Cenas/Stage2/game.js
@@ -256,57 +256,41 @@ class GameEngine {
         firstFrame: true,
         keyframe: 0,
         points: [
-          { id: 0, color: { r: 0, g: 0, b: 0 }, position: 0 },
-          { id: 1, color: { r: 16, g: 30, b: 92 }, position: 0.07 },
-          { id: 2, color: { r: 40, g: 124, b: 144 }, position: 0.14 },
-          { id: 3, color: { r: 42, g: 21, b: 79 }, position: 0.48 },
-          { id: 4, color: { r: 13, g: 152, b: 104 }, position: 0.81 },
-          { id: 5, color: { r: 11, g: 50, b: 25 }, position: 1 },
-          { id: 6, color: { r: 11, g: 48, b: 50 }, position: 1 },
-          { id: 7, color: { r: 200, g: 60, b: 255 }, position: 1 },
-          { id: 8, color: { r: 253, g: 60, b: 255 }, position: 1 },
+          { id: 0, color: { r: 140, g: 148, b: 239 }, position: 0 },
+          { id: 1, color: { r: 140, g: 165, b: 239 }, position: 0.33 },
+          { id: 2, color: { r: 140, g: 170, b: 239 }, position: 1 },
         ]
       },
       {
-        keyframe: 1000, //duration
+        keyframe: 500,
         points: [
-          { id: 0, color: { r: 0, g: 0, b: 0 }, position: 0 },
-          { id: 1, color: { r: 16, g: 30, b: 92 }, position: 0.01 },
-          { id: 2, color: { r: 40, g: 124, b: 144 }, position: 0.02 },
-          { id: 3, color: { r: 42, g: 21, b: 79 }, position: 0.03 },
-          { id: 4, color: { r: 13, g: 52, b: 104 }, position: 0.04 },
-          { id: 5, color: { r: 13, g: 125, b: 152 }, position: 0.10 },
-          { id: 6, color: { r: 10, g: 23, b: 106 }, position: 0.25 },
-          { id: 7, color: { r: 253, g: 60, b: 55 }, position: 0.53 },
-          { id: 8, color: { r: 200, g: 60, b: 255 }, position: 1 },
+          { id: 0, color: { r: 189, g: 170, b: 239 }, position: 0 },
+          { id: 1, color: { r: 206, g: 180, b: 239 }, position: 0.33 },
+          { id: 2, color: { r: 206, g: 197, b: 239 }, position: 1 },
         ]
       },
       {
-        keyframe: 3000, //duration
+        keyframe: 1000,
         points: [
-          { id: 0, color: { r: 0, g: 0, b: 0 }, position: 0 },
-          { id: 1, color: { r: 16, g: 30, b: 92 }, position: 0.001 },
-          { id: 2, color: { r: 40, g: 124, b: 144 }, position: 0.002 },
-          { id: 3, color: { r: 42, g: 21, b: 79 }, position: 0.003 },
-          { id: 4, color: { r: 97, g: 24, b: 25 }, position: 0.06 },
-          { id: 5, color: { r: 40, g: 52, b: 131 }, position: 0.25 },
-          { id: 6, color: { r: 209, g: 38, b: 238 }, position: 0.42 },
-          { id: 7, color: { r: 226, g: 14, b: 164 }, position: 0.78 },
-          { id: 8, color: { r: 97, g: 24, b: 25 }, position: 1 },
+          { id: 0, color: { r: 224, g: 225, b: 220 }, position: 0 },
+          { id: 1, color: { r: 239, g: 239, b: 206 }, position: 0.33 },
+          { id: 2, color: { r: 239, g: 235, b: 169 }, position: 1 },
         ]
       },
       {
-        keyframe: 5000, //duration
+        keyframe: 1300,
         points: [
-          { id: 0, color: { r: 0, g: 0, b: 0 }, position: 0 },
-          { id: 1, color: { r: 16, g: 30, b: 92 }, position: 0.001 },
-          { id: 2, color: { r: 40, g: 124, b: 144 }, position: 0.002 },
-          { id: 3, color: { r: 42, g: 21, b: 79 }, position: 0.003 },
-          { id: 4, color: { r: 70, g: 15, b: 162 }, position: 0.11 },
-          { id: 5, color: { r: 79, g: 27, b: 113 }, position: 0.31 },
-          { id: 6, color: { r: 90, g: 124, b: 136 }, position: 0.36 },
-          { id: 7, color: { r: 85, g: 24, b: 93 }, position: 0.6 },
-          { id: 8, color: { r: 50, g: 7, b: 15 }, position: 1 },
+          { id: 0, color: { r: 239, g: 212, b: 145 }, position: 0 },
+          { id: 1, color: { r: 239, g: 191, b: 122 }, position: 0.33 },
+          { id: 2, color: { r: 239, g: 182, b: 113 }, position: 1 },
+        ]
+      },
+      {
+        keyframe: 2000,
+        points: [
+          { id: 0, color: { r: 239, g: 154, b: 119 }, position: 0 },
+          { id: 1, color: { r: 239, g: 145, b: 130 }, position: 0.33 },
+          { id: 2, color: { r: 239, g: 138, b: 144 }, position: 1 },
         ]
       },
 
@@ -451,8 +435,8 @@ class GameEngine {
     this.stageBuilder = new ChallengeDynamicBuilder({
       currentSpeed: 1,
       speedStep: 0.15,
-      param1Range: [4, 5, 3, 3, 4, 3, 4], // Na fase 3 o barato é louco
-      param2Range: [9, 4, 3, 6, 8, 5],
+      param1Range: [6, 5, 7, 6, 5, 7], // Na fase 3 o barato é louco
+      param2Range: [6, 9, 3, 4, 7, 5],
       currentCheckpoints: initialCheckpoints,
       currentCheckpointCount: 0,
       chooseNextChallenge: () => {
