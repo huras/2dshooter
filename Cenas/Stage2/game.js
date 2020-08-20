fc1a2c442a5f5ad29e6b416c4ef46c1e25c87933
jean <jean@criasol>
2020-08-20 16:08:56 -0300

    cores stage2

--- Cenas/Stage2/game.js
+++ Cenas/Stage2/game.js
@@ -256,43 +256,45 @@ class GameEngine {
         firstFrame: true,
         keyframe: 0,
         points: [
-          { id: 0, color: { r: 200, g: 127, b: 247 }, position: 0 },
-          { id: 1, color: { r: 200, g: 127, b: 247 }, position: 0.14 },
-          { id: 2, color: { r: 184, g: 110, b: 244 }, position: 0.24 },
-          { id: 3, color: { r: 184, g: 110, b: 244 }, position: 0.34 },
-          { id: 4, color: { r: 167, g: 91, b: 236 }, position: 0.54 },
-          { id: 5, color: { r: 139, g: 67, b: 225 }, position: 0.65 },
-          { id: 6, color: { r: 117, g: 52, b: 213 }, position: 0.78 },
-          { id: 7, color: { r: 97, g: 42, b: 199 }, position: 0.88 },
-          { id: 8, color: { r: 97, g: 42, b: 199 }, position: 1 },
+          { id: 0, color: { r: 0, g: 0, b: 0 }, position: 0 },
+          { id: 1, color: { r: 29, g: 130, b: 61 }, position: 0.15 },
+          { id: 2, color: { r: 129, g: 213, b: 43 }, position: 0.44 },
+          { id: 3, color: { r: 10, g: 105, b: 28 }, position: 1 },
+          { id: 4, color: { r: 10, g: 105, b: 28 }, position: 1 },
+          { id: 5, color: { r: 10, g: 105, b: 28 }, position: 1 },
         ]
       },
       {
-        keyframe: 4000, //duration
+        keyframe: 2000,
         points: [
-          { id: 0, color: { r: 69, g: 29, b: 175 }, position: 0 },
-          { id: 1, color: { r: 69, g: 29, b: 175 }, position: 0.14 },
-          { id: 2, color: { r: 51, g: 23, b: 154 }, position: 0.24 },
-          { id: 3, color: { r: 51, g: 23, b: 154 }, position: 0.34 },
-          { id: 4, color: { r: 35, g: 17, b: 127 }, position: 0.54 },
-          { id: 5, color: { r: 35, g: 17, b: 127 }, position: 0.65 },
-          { id: 6, color: { r: 25, g: 11, b: 106 }, position: 0.78 },
-          { id: 7, color: { r: 25, g: 11, b: 106 }, position: 0.88 },
-          { id: 8, color: { r: 18, g: 9, b: 88 }, position: 1 },
-        ]
-      },
-      {
-        keyframe: 4500, //duration
-        points: [
-          { id: 0, color: { r: 18, g: 6, b: 67 }, position: 0 },
-          { id: 1, color: { r: 3, g: 2, b: 33 }, position: 0.25 },
-          { id: 2, color: { r: 0, g: 1, b: 11 }, position: 0.5 },
-          { id: 3, color: { r: 0, g: 0, b: 0 }, position: 1 },
-          { id: 4, color: { r: 0, g: 0, b: 0 }, position: 1 },
+          { id: 0, color: { r: 0, g: 0, b: 0 }, position: 0 },
+          { id: 1, color: { r: 11, g: 51, b: 24 }, position: 0.1 },
+          { id: 2, color: { r: 95, g: 167, b: 20 }, position: 0.37 },
+          { id: 3, color: { r: 22, g: 179, b: 51 }, position: 0.65 },
+          { id: 4, color: { r: 0, g: 93, b: 17 }, position: 0.93 },
           { id: 5, color: { r: 0, g: 0, b: 0 }, position: 1 },
-          { id: 6, color: { r: 0, g: 0, b: 0 }, position: 1 },
-          { id: 7, color: { r: 0, g: 0, b: 0 }, position: 1 },
-          { id: 8, color: { r: 0, g: 0, b: 0 }, position: 1 },
+        ]
+      },
+      {
+        keyframe: 4000,
+        points: [
+          { id: 0, color: { r: 0, g: 0, b: 0 }, position: 0 },
+          { id: 1, color: { r: 3, g: 36, b: 14 }, position: 0.07 },
+          { id: 2, color: { r: 69, g: 121, b: 15 }, position: 0.34 },
+          { id: 3, color: { r: 106, g: 149, b: 76 }, position: 0.57 },
+          { id: 4, color: { r: 38, g: 110, b: 51 }, position: 0.75 },
+          { id: 5, color: { r: 0, g: 0, b: 0 }, position: 1 },
+        ]
+      },
+      {
+        keyframe: 6000,
+        points: [
+          { id: 0, color: { r: 255, g: 0, b: 0 }, position: 0 },
+          { id: 1, color: { r: 255, g: 130, b: 61 }, position: 0.15 },
+          { id: 2, color: { r: 255, g: 213, b: 43 }, position: 0.44 },
+          { id: 3, color: { r: 255, g: 105, b: 28 }, position: 1 },
+          { id: 4, color: { r: 255, g: 105, b: 28 }, position: 1 },
+          { id: 5, color: { r: 255, g: 105, b: 28 }, position: 1 },
         ]
       },
     ], this.canvas, this.ctx);
